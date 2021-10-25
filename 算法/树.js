56bd299245383c5b0bf28611517265e5320a27ce
woqi <741644017@qq.com>
2021-10-26 00:54:36 +0800

    ~

--- 算法/树.js
+++ 算法/树.js
@@ -38,12 +38,80 @@ const dfs = root => {
 // dfs(tree)
 
 const bfs = root => {
+  //广度优先遍历
   const q = [root]
   while (q.length > 0) {
-    const n = q.shift() //剔除队头
+    const n = q.shift() //队头c出队
     console.log(n.val)
-    n.children.forEach(item => p.push(item))
+    n.children.forEach(item => q.push(item))
   }
 }
+// bfs(tree)
 
 //
+//二叉树部分
+const bt = {
+  val: 1,
+  left: {
+    val: 2,
+    left: {
+      val: 4,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 5,
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    val: 3,
+    left: {
+      val: 6,
+      left: null,
+      right: null,
+    },
+    right: {
+      val: 7,
+      left: null,
+      right: null,
+    },
+  },
+}
+
+// module.exports = bt;
+
+//先序遍历
+const preorder = root => {
+  console.log(root.val)
+  if (root != null) {
+    if (root.left) {
+      preorder(root.left)
+    }
+    if (root.right) {
+      preorder(root.right)
+    }
+  }
+}
+// preorder(bt)//1245367
+//中序遍历
+const inorder = root => {
+  if (!root) {
+    return
+  }
+  inorder(root.left)
+  console.log(root.val)
+  inorder(root.right)
+}
+// inorder(bt) //4251637
+
+const postorder = root => {
+  if (!root) {
+    return
+  }
+  postorder(root.left)
+  postorder(root.right)
+  console.log(root.val)
+}
+// postorder(bt)//4526731
