035425b76d69c55b69d27d55819933bb9829b170
woqi <741644017@qq.com>
2021-11-19 00:29:59 +0800

    ~~

--- 简答题2.js
+++ 简答题2.js
@@ -28,15 +28,15 @@ async function show2() {
   let data1 = await request('/data1')
   let data2 = await request('/data2')
   let data3 = await request('/data3')
-  console.log(data1, data2, data3)
+  // console.log(data1, data2, data3)
   return 55
 }
 function show2() {
-  return new Promise((resolve, reject)=>{
+  return new Promise((resolve, reject) => {
     console.log(12)
-    request('/data1').then(data1=>{
-      request('/data2').then(data2=>{
-        request('/data3').then(data3=>{
+    request('/data1').then(data1 => {
+      request('/data2').then(data2 => {
+        request('/data3').then(data3 => {
           console.log(data1, data2, data3)
           resolve(55)
         })
@@ -44,3 +44,54 @@ function show2() {
     })
   })
 }
+// var a = {},
+//   b = '123',
+//   c = 123
+// a[b] = 'b'
+// a[c] = 'c'
+// console.log(a[b])
+
+let json = {}
+json[111] = 'adsa'
+
+for (let key in json) {
+  console.log(typeof key)
+}
+let map = new Map()
+map.set('1', 'dd')
+map.set(1, 'dd')
+// console.log('m~~~~~~~~~',map)
+
+// var a = {},
+//   b = Symbol('123'),
+//   c = Symbol('123')
+// a[b] = 'b'
+// a[c] = 'c'
+// console.log(a[b])
+
+// var a = {},
+//   b = {key:'123'},
+//   c = {key:'456'}
+// a[b] = 'b'
+// a[c] = 'c'
+// console.log(a[b])
+
+//两数之和
+
+let nums = [2, 7, 11, 15],
+  target = 13
+function Two(nums, target) {
+  const obj = {}
+  for (let i = 0; i < nums.length; i++) {
+    let a = nums[i]
+    let b = target - a
+    if (obj[b] != undefined) {
+      return [obj[b], i]
+    } else {
+      obj[a] = i
+    }
+  }
+  return null
+}
+//时间复杂度和空间复杂度都是O(n)
+console.log('~~~~', Two(nums, target))
